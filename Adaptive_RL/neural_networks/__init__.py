fc0bd6e2e61906e5e45538227321999ba92ded8a
YusseffRuiz <zeiverking@gmail.com>
2024-10-01 17:49:06 -0300

    SGD and ARS Implementation.

--- Adaptive_RL/neural_networks/__init__.py
+++ Adaptive_RL/neural_networks/__init__.py
@@ -1,3 +1,3 @@
-from .actors import ActorCriticWithTargets, ActorTwinCriticWithTargets, DeterministicPolicyGradient, TwinCriticSoftDeterministicPolicyGradient, MaximumAPosterioriPolicyOptimization, ClippedRatio, ActorCritic
+from .actors import ActorCriticWithTargets, ActorTwinCriticWithTargets, DeterministicPolicyGradient, TwinCriticSoftDeterministicPolicyGradient, MaximumAPosterioriPolicyOptimization, ClippedRatio, ActorCritic, StochasticPolicyGradient
 from .base_network import BaseModel, ActorTwinCriticsModelNetwork, ActorCriticModelNetwork
 from .critics import ExpectedSARSA, DeterministicQLearning, TwinCriticSoftQLearning, VRegression
